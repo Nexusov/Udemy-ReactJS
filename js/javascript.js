01667fdfd31d69f1e76b47843604ac5527220fb8
Nexus <egor2001grashin@gmail.com>
2022-07-09 15:56:01 +0300

    test

--- js/javascript.js
+++ js/javascript.js
@@ -1,2 +1,13 @@
-let name = 'Alex'
-console.log(`My name is ${name}`)
+
+let BBB = ['Marshak', 'Dmax', 'Vangard']
+
+BBB.forEach(element => {
+    element = element + ' глупый' 
+});
+
+console.log(BBB)
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+}
+
