eb68c028c671e0d0d51a3a36d3fad0d52ffaf278
Nexus <egor2001grashin@gmail.com>
2022-07-30 19:20:03 +0300

    transfered code about child and parent

--- js/testDOM.js
+++ js/testDOM.js
@@ -150,3 +150,32 @@ btns.forEach(button => {
 
 
 addEventListener('click', deletElement, {once: true}) // {once: true} - говорит, что выполнить только 1 раз. Можно не цикл со счетчиком и удалять руками
+
+/* child and parent */
+
+console.log(document.body.childNodes) // childNodes возвращает коллекцию дочерних элементов данного элемента.
+
+console.log(document.body.firstChild) // получаем первый элемент страницы из нод-списка
+console.log(document.body.firstElementChild) // получаем первый элемент страницы
+console.log(document.body.lastChild)  // получаем последний элемент страницы из нод-списка
+console.log(document.body.lastElementChild)  // получаем последний элемент страницы
+
+console.log(document.querySelector('#current').parentNode) // .parentNode возвращает родителя определённого элемента DOM дерева.
+console.log(document.querySelector('#current').parentNode.parentNode) // поднимаемся выше
+
+console.log(document.querySelector('#current').parentElement) //  возвращает элемент-родитель определённого элемента DOM дерева !! ИМЕННО ЭЛЕМЕНТ
+
+console.log(document.querySelector('[data-current = "3"]')) // [] - получаем data-attribute
+console.log(document.querySelector('[data-current = "3"]').nextSibling)  // nextSibling возвращает узел, непосредственно следующий за данным узлом в списке childNodes его родительского элемента
+console.log(document.querySelector('[data-current = "3"]').previousSibling)  // получаем предыдущего "соседа"
+
+
+console.log(document.querySelector('[data-current = "3"]').nextElementSibling)  // получаем след элемент !! ИМЕННО ЭЛЕМЕНТ
+console.log(document.querySelector('[data-current = "3"]').previousElementSibling)  // получаем пред элемент !! ИМЕННО ЭЛЕМЕНТ
+
+for (let node of document.body.childNodes) { // избавляемся от текстовых нод
+    if (node.nodeName == '#text') {
+        continue;
+    }
+    console.log(node)
+}
