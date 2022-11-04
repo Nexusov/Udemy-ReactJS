3bec9a7265e08f7dee1f5af73e167b02a5dbad54
Nexus <egor2001grashin@gmail.com>
2022-11-04 20:33:40 +0300

    made tasks with array withods

--- js/tasks.js
+++ js/tasks.js
@@ -1,3 +1,4 @@
+import current from '../calculator/calc/js/current.json';
 /* 
 Написать код, который нарисует пирамидку из звездочек
 */
@@ -302,5 +303,110 @@ function transferWaitors(data) {
 
 console.log(transferWaitors(restorantData))
 
+
+
 /* ======================================================================== */
 
+
+/* ================================= Movies ======================================= */
+
+const films = [
+    {
+        name: 'Titanic',
+        rating: 9
+    },
+    {
+        name: 'Die hard 5',
+        rating: 5
+    },
+    {
+        name: 'Matrix',
+        rating: 8
+    },
+    {
+        name: 'Some bad film',
+        rating: 4
+    }
+];
+
+
+/* 
+    У вас есть список фильмов с рейтингом в виде массива объектов. Напишите функцию showGoodFilms, которая будет принимать этот массив, 
+    а возвращать будет массив объектов только с теми фильмами, у которых рейтинг больше или равен 8.
+*/
+
+function showGoodFilms(arr) {
+	return arr.filter(item => item.rating >= 8)
+}
+
+
+/* 
+    Напишите функцию showListOfFilms, которая будет принимать этот же массив, а возвращать будет строку, которая содержит названия фильмов через запятую.
+*/
+
+function showListOfFilms(arr) {
+	return arr.map(item => item.name).join(', ')
+}
+
+
+/* 
+    Напишите функцию setFilmsIds, которая будет принимать этот же массив, а возвращать будет такой же массив с фильмами, 
+    но у каждого фильма будет новое поле id. Значение этого поля установите по нумерации фильма.
+*/
+
+function setFilmsIds(arr) {
+    return arr.map((item, i) => {
+        item.id = i
+        return item
+    })
+}
+
+
+/* 
+    Запишите результат предыдущей функции в переменную tranformedArray. 
+    Напишите функцию checkFilms, которая будет проверять, что в каждом из фильмов есть поле id. Если это так - функция возвращает true
+*/
+
+
+const tranformedArray = setFilmsIds(films);
+
+function checkFilms(arr) {
+    return arr.every(item => ('id' in item))
+}
+
+/* ======================================================================== */
+
+
+/* ============================== Markets ========================================== */
+
+const funds = [
+    {amount: -1400},
+    {amount: 2400},
+    {amount: -1000},
+    {amount: 500},
+    {amount: 10400},
+    {amount: -11400}
+];
+
+
+/* 
+    У вас есть небольшой массив с данными о доходах каждой торговой точки. 
+    Напишите функцию getPositiveIncomeAmount, которая принимает этот массив данных и возвращает сумму только положительных значений из каждого объекта. (число)
+*/
+
+const getPositiveIncomeAmount = (data) => {
+    return data.filter(item => item.amount > 0).reduce((acc, current) => acc + current.amount, 0)
+};
+
+
+/* 
+    Напишите функцию getTotalIncomeAmount, которая тоже принимает этот массив данных. 
+    Если хотя бы один из объектов содержит отрицательное значение поля amount, то функция возвращает сумму всех значений (число). 
+    Если таких значений нет - запускается функция getPositiveIncomeAmount с тем же массивом данных.
+*/
+
+const getTotalIncomeAmount = (data) => {
+    return data.some(item => item.amount < 0) ? data.reduce((acc, current) => acc + current.amount, 0) : getPositiveIncomeAmount(data)
+};
+
+/* ======================================================================== */
