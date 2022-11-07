9405d333d945e08b58fb1451005bdeaf37f52211
Nexus <egor2001grashin@gmail.com>
2022-11-07 18:36:28 +0300

    simple slider for pictures

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -345,6 +345,56 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
 
+    // Slider
+
+    const slides = document.querySelectorAll('.offer__slide')
+    const prev = document.querySelector('.offer__slider-prev')
+    const next = document.querySelector('.offer__slider-next')
+
+    const total = document.querySelector('#total')
+    const current = document.querySelector('#current')
+
+    let slideIndex = 1
+
+    showSlides(slideIndex)
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`
+    } else {
+        total.textContent = slides.length
+    }
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length
+        }
+
+        slides.forEach(item => item.style.display = 'none')
+
+        slides[slideIndex - 1].style.display = 'block'
+
+        if (n < 10) {
+            current.textContent = `0${slideIndex}`
+        } else {
+            current.textContent = slideIndex
+        }    
+    }
+
+    function plussSlides(n) {
+        showSlides(slideIndex += n)
+    }
+
+    prev.addEventListener('click', () => {
+        plussSlides(-1)
+    })
+
+    next.addEventListener('click', () => {
+        plussSlides(1) 
+    })
 
 
 
