82c7ccf874ad94cc5c0f96d6ac715dd6dca154ea
Nexus <egor2001grashin@gmail.com>
2022-11-19 14:04:55 +0300

    Elements & Components in React

--- Learning React/testapp/src/App.js
+++ Learning React/testapp/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Header = () => {
+	return <h2>Hello World!</h2>;
+};
+
+const Field = () => {
+   const holder = 'Enter here'
+   const styledField = {
+      width: '300px'
+   }
+
+	return <input placeholder={holder} type='text' style={styledField} />;
+};
+
+class FieldClass extends Component { // Class component syntax
+   render() {
+      const holder = 'Enter here'
+      const styledField = {
+         width: '300px'
+      }
+
+      return <input placeholder={holder} type='text' style={styledField} />; 
+   }
 }
 
+function Btn() {
+   const text = 'Log in';
+   const isLogged = false;
+
+	return <button>{isLogged ? 'Enter' : text}</button>;
+}
+
+function App() {
+	return (
+		<div className='App'>
+			<Header />
+			<Field />
+			<Btn />
+		</div>
+	);
+}
+
+export {Header}
 export default App;
