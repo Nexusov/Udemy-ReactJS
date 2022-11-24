34a08e4ae31055e8e0ca631a7fb411dac8119fe3
Nexus <egor2001grashin@gmail.com>
2022-11-24 14:51:25 +0300

    state in React | setState()

--- Learning React/testapp/src/App.js
+++ Learning React/testapp/src/App.js
@@ -33,12 +33,43 @@ function Btn() {
 	return <button>{isLogged ? 'Enter' : text}</button>;
 }
 
+class WhoAmI extends Component {
+   constructor(props) {
+      super(props)
+      this.state = {
+         years: 27,
+         text: 'TEXT'
+      }
+   }
+
+   nextYear = () => {
+      this.setState(state => ({
+         years: state.years + 1
+      }))
+   }
+
+	render() {
+      const {name, surname, link} = this.props
+		return (
+			<div>
+            <button onClick={this.nextYear}>{this.state.text}</button>
+				<h1>
+					My name is {name}, surname - {surname}, age - {this.state.years}
+				</h1>
+				<a href={link}>My profile</a>
+			</div>
+		);
+	}
+}
+
 function App() {
 	return (
 		<div className='App'>
 			<Header />
 			<Field />
 			<Btn />
+         <WhoAmI name='John' surname='Shepard' link='facebook.com' />
+         <WhoAmI name='Alex' surname='Smith' link='twitter.com' />
 		</div>
 	);
 }
