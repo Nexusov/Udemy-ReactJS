91556f10068aaaf64a737464d788198badff9e07
Nexus <egor2001grashin@gmail.com>
2022-11-29 18:00:49 +0300

    work with marvel's API | fetch requests

--- Learning React/marvelproject/src/index.jsx
+++ Learning React/marvelproject/src/index.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './components/App/App';
+import MarvelService from './services/MarvelService';
 
 import './style/style.scss';
+
+const marvelService = new MarvelService()
+marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ Learning React/marvelproject/src/services/MarvelService.jsx
@@ -0,0 +1,27 @@
+
+
+class MarvelService {
+
+   _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+   _apiKey = 'apikey=2ae406f8b04bae4282a528673f9eec97'
+
+	getResource = async (url) => {
+		let res = await fetch(url);
+
+		if (!res.ok) {
+			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+		}
+
+		return await res.json();
+	};
+
+   getAllCharacters = () => {
+      return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`)
+   }
+
+   getCharacter = (id) => {
+      return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`)
+   }
+}
+
+export default MarvelService
