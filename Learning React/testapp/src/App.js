fffb01e07b22ec0aaf67bbfaa41ed290d2f7e902
Nexus <egor2001grashin@gmail.com>
2022-12-01 01:38:47 +0300

    testing render-props pattern in react

--- Learning React/testapp/src/App.js
+++ Learning React/testapp/src/App.js
@@ -138,9 +138,44 @@ const HelloGreating = () => {
    )
 }
 
+const Message = (props) => {
+   return (
+      <h2>
+         The counter is {props.counter}
+      </h2>
+   )
+}
+
+class Counter extends Component {
+	state = {
+		counter: 0,
+	};
+
+	changeCounter = () => {
+		this.setState(({ counter }) => ({
+			counter: counter + 1,
+		}));
+	};
+
+	render() {
+		return (
+			<>
+				<button
+					className={'btn btn-primary'}
+					onClick={this.changeCounter}
+				>
+					Click me!
+				</button>
+            {this.props.render(this.state.counter)}
+			</>
+		);
+	}
+}
+
 function App() {
 	return (
 		<Wrapper>
+         <Counter render={counter => (<Message counter={counter} />)}/>
 			<Header />
 			<Field />
 			<Btn />
