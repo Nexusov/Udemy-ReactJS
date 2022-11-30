dfa9619308cd46c82ef6f020a8261e39276ba16c
Nexus <egor2001grashin@gmail.com>
2022-11-30 21:01:01 +0300

    Error Boundaries in react

--- Learning React/marvelproject/src/components/App/App.jsx
+++ Learning React/marvelproject/src/components/App/App.jsx
@@ -4,35 +4,38 @@ import AppHeader from '../AppHeader/AppHeader';
 import RandomChar from '../RandomChar/RandomChar';
 import CharList from '../CharList/CharList';
 import CharInfo from '../CharInfo/CharInfo';
+import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 
 import decoration from '../../resources/img/vision.png';
-
 
 class App extends Component {
 	state = {
 		selectedCharacter: null,
-	}
+	};
 
 	onCharacterSelected = (id) => {
 		this.setState({
-			selectedCharacter: id
-		})
-	}
-	
+			selectedCharacter: id,
+		});
+	};
+
 	render() {
 		return (
 			<div className='app'>
 				<AppHeader />
 				<main>
-					<RandomChar />
+					<ErrorBoundary>
+						<RandomChar />
+					</ErrorBoundary>
 					<div className='char__content'>
-						<CharList onCharacterSelected={this.onCharacterSelected}/>
-						<CharInfo characterId={this.state.selectedCharacter}/>
+						<ErrorBoundary>
+							<CharList onCharacterSelected={this.onCharacterSelected} />
+						</ErrorBoundary>
+						<ErrorBoundary>
+							<CharInfo characterId={this.state.selectedCharacter} />
+						</ErrorBoundary>
 					</div>
-					<img
-						className='bg-decoration'
-						src={decoration}
-						alt='vision'
+					<img className='bg-decoration' src={decoration} alt='vision'
 					/>
 				</main>
 			</div>
