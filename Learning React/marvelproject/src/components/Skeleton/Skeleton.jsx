4438450aa4803322300b00db631c417a0e9af9bc
Nexus <egor2001grashin@gmail.com>
2022-11-30 19:48:22 +0300

    fixed typo

--- Learning React/marvelproject/src/components/Skeleton/Skeleton.jsx
+++ Learning React/marvelproject/src/components/Skeleton/Skeleton.jsx
@@ -3,7 +3,7 @@ import './skeleton.scss';
 const Skeleton = () => {
     return (
         <>
-            <p className="char__select">Please select a character to see information</p>
+            <p className="char__select">Please select a character to see the information</p>
             <div className="skeleton">
                 <div className="pulse skeleton__header">
                     <div className="pulse skeleton__circle"></div>
