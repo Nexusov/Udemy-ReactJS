1a1065cf070b28c3220c0231bb10768411416511
Nexus <egor2001grashin@gmail.com>
2023-01-26 21:34:35 +0300

    made homework

--- Learning Redux/hero_admin_panel/src/actions/index.js
+++ Learning Redux/hero_admin_panel/src/actions/index.js
@@ -16,3 +16,55 @@ export const heroesFetchingError = () => {
       type: 'HEROES_FETCHING_ERROR'
    }
 }
+
+export const heroesDeletingError = () => {
+   return {
+      type: 'HEROES_DELETING_ERROR'
+   }
+}
+
+export const heroDeleted = (id) => {
+   return {
+      type: 'HERO_DELETED',
+      payload: id
+   }
+}
+
+export const heroCreated = (hero) => {
+   return {
+      type: 'HERO_CREATED',
+      payload: hero
+   }
+}
+
+export const heroesCreatingError = () => {
+   return {
+      type: 'HEROES_CREATING_ERROR'
+   }
+}
+
+export const activeFilterChanged = (filter) => {
+   return {
+      type: 'ACTIVE_FILTER_CHANGED',
+      payload: filter
+   }
+}
+
+export const filtersFetching = () => {
+   return {
+      type: 'FILTERS_FETCHING'
+   }
+}
+
+export const filtersFetched = (filter) => {
+   return {
+      type: 'FILTERS_FETCHED',
+      payload: filter
+   }
+}
+
+export const filtersFetchingError = () => {
+   return {
+      type: 'FILTERS_FETCHING_ERROR'
+   }
+}
--- Learning Redux/hero_admin_panel/src/components/heroesList/HeroesList.jsx
+++ Learning Redux/hero_admin_panel/src/components/heroesList/HeroesList.jsx
@@ -1,15 +1,25 @@
-import {useHttp} from '../../hooks/http.hook';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-
-import { heroesFetching, heroesFetched, heroesFetchingError } from '../../actions';
-import HeroesListItem from "../heroesListItem/HeroesListItem";
-import Spinner from '../spinner/Spinner';
-
 // Задача для этого компонента:
 // При клике на "крестик" идет удаление персонажа из общего состояния
 // Усложненная задача:
 // Удаление идет и с json файла при помощи метода DELETE
+
+import {useHttp} from '../../hooks/http.hook';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { CSSTransition, TransitionGroup} from 'react-transition-group';
+
+import {
+   heroesFetching,
+   heroesFetched,
+   heroesFetchingError,
+   heroDeleted,
+   heroesDeletingError
+} from '../../actions';
+import HeroesListItem from "../heroesListItem/HeroesListItem";
+import Spinner from '../spinner/Spinner';
+import { useCallback } from 'react';
+
+import './heroesList.scss';
 
 const HeroesList = () => {
    const {heroes, heroesLoadingStatus} = useSelector(state => state);
@@ -19,11 +29,19 @@ const HeroesList = () => {
    useEffect(() => {
       dispatch(heroesFetching());
       request("http://localhost:3001/heroes")
-            .then(data => dispatch(heroesFetched(data)))
-            .catch(() => dispatch(heroesFetchingError()))
+         .then(data => dispatch(heroesFetched(data)))
+         .catch(() => dispatch(heroesFetchingError()))
 
       // eslint-disable-next-line
    }, []);
+
+   const onDelete = useCallback((id) => {
+      request(`http://localhost:3001/heroes/${id}`, 'DELETE')
+         .then(dispatch(heroDeleted(id)))
+         .catch(() => dispatch(heroesDeletingError()))
+
+      // eslint-disable-next-line
+   }, [request])
 
    if (heroesLoadingStatus === "loading") {
       return <Spinner/>;
@@ -33,19 +51,33 @@ const HeroesList = () => {
 
    const renderHeroesList = (arr) => {
       if (arr.length === 0) {
-         return <h5 className="text-center mt-5">Героев пока нет</h5>
+         return (
+            <CSSTransition
+                  timeout={0}
+                  classNames="hero">
+                  <h5 className="text-center mt-5">Героев пока нет</h5>
+            </CSSTransition>
+         )
       }
 
       return arr.map(({id, ...props}) => {
-         return <HeroesListItem key={id} {...props}/>
+         return (
+            <CSSTransition 
+               key={id}
+               timeout={500}
+               classNames="hero">
+               <HeroesListItem  {...props} onDelete={() => onDelete(id)}/>
+            </CSSTransition>
+         )
       })
    }
-
+   
    const elements = renderHeroesList(heroes);
+
    return (
-      <ul>
+      <TransitionGroup component="ul">
          {elements}
-      </ul>
+      </TransitionGroup>
    )
 }
 
