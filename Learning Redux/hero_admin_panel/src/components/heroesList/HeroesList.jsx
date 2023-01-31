45a66b3664bd766303abffe5ff987c5518b7b8ce
Nexus <egor2001grashin@gmail.com>
2023-01-31 23:27:56 +0300

    store enhancers

--- Learning Redux/hero_admin_panel/src/components/heroesList/HeroesList.jsx
+++ Learning Redux/hero_admin_panel/src/components/heroesList/HeroesList.jsx
@@ -50,7 +50,7 @@ const HeroesList = () => {
    const {request} = useHttp();
 
    useEffect(() => {
-      dispatch(heroesFetching());
+      dispatch('HEROES_FETCHING');
       request("http://localhost:3001/heroes")
          .then(data => dispatch(heroesFetched(data)))
          .catch(() => dispatch(heroesFetchingError()))
