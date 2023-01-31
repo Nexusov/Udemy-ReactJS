1e0f2636f7db97bee4be2092fae70eb737d21707
Nexus <egor2001grashin@gmail.com>
2023-02-01 00:12:34 +0300

    made redux-thunk for filters

--- Learning Redux/hero_admin_panel/src/components/heroesFilters/HeroesFilters.jsx
+++ Learning Redux/hero_admin_panel/src/components/heroesFilters/HeroesFilters.jsx
@@ -11,7 +11,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames';
 
-import { filtersFetching, filtersFetched, filtersFetchingError, activeFilterChanged } from '../../actions';
+import { fetchFilters, activeFilterChanged } from '../../actions';
 import Spinner from '../spinner/Spinner';
 
 const HeroesFilters = () => {
@@ -21,10 +21,7 @@ const HeroesFilters = () => {
    const {request} = useHttp();
 
    useEffect(() => {
-      dispatch(filtersFetching());
-      request("http://localhost:3001/filters")
-         .then(data => dispatch(filtersFetched(data)))
-         .catch(() => dispatch(filtersFetchingError()))
+      dispatch(fetchFilters(request));
 
       // eslint-disable-next-line
    }, []);
