3435ad5ccef094cd90f190a03c242659d0e08d7f
Nexus <egor2001grashin@gmail.com>
2023-01-31 23:42:40 +0300

    redux middleware

--- Learning Redux/hero_admin_panel/src/store/index.js
+++ Learning Redux/hero_admin_panel/src/store/index.js
@@ -1,6 +1,15 @@
-import { createStore, combineReducers, compose} from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
+
+const stringMiddleware = () => (next) => (action) => {
+   if (typeof action === 'string') {
+      return next({
+         type: action
+      })
+   }
+   return next(action)
+}
 
 const enhancer = (createStore) => (...args) => {
    const store = createStore(...args)
@@ -17,6 +26,6 @@ const enhancer = (createStore) => (...args) => {
    return store
 }
 
-const store = createStore(combineReducers({heroes, filters}), compose(enhancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(combineReducers({heroes, filters}), compose(applyMiddleware(stringMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 export default store;
