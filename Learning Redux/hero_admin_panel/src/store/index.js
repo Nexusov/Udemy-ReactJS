6a02aade94b1e2685953eab9be8a1d23a0a75d01
Nexus <egor2001grashin@gmail.com>
2023-02-01 14:39:53 +0300

    using configureStore from Redux Toolkit

--- Learning Redux/hero_admin_panel/src/store/index.js
+++ Learning Redux/hero_admin_panel/src/store/index.js
@@ -1,3 +1,4 @@
+import { configureStore } from '@reduxjs/toolkit'
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk'
 import heroes from '../reducers/heroes';
@@ -27,6 +28,12 @@ const enhancer = (createStore) => (...args) => {
    return store
 }
 
-const store = createStore(combineReducers({heroes, filters}), compose(applyMiddleware(ReduxThunk, stringMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+// const store = createStore(combineReducers({heroes, filters}), compose(applyMiddleware(ReduxThunk, stringMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())); 
+
+const store = configureStore({
+   reducer: {heroes, filters},
+   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+   devTools: process.env.NODE_ENV !== 'production',
+})
 
 export default store;
