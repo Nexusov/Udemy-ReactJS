08002277e7b084bda85d7d6911a827a62263d1bd
Nexus <egor2001grashin@gmail.com>
2023-02-16 05:58:35 +0300

    made calculator-form alive

--- Learning JS/IrvasWindowsProject/src/js/modules/forms.js
+++ Learning JS/IrvasWindowsProject/src/js/modules/forms.js
@@ -1,13 +1,10 @@
-const forms = () => {
+import checkNumInputs from './checkNumInputs';
+
+const forms = (state) => {
    const form = document.querySelectorAll('form')
    const inputs = document.querySelectorAll('input')
-   const phoneInputs = document.querySelectorAll('input[name="user_phone"]')
 
-   phoneInputs.forEach(item => {
-      item.addEventListener('input', () => {
-         item.value = item.value.replace(/\D/, '')
-      })
-   })
+   checkNumInputs('input[name="user_phone"]')
 
    const message = {
       loading: 'Загрузка',
@@ -40,6 +37,11 @@ const forms = () => {
          item.appendChild(statusMessage)
 
          const formData = new FormData(item)
+         if (item.getAttribute('data-calc') === 'end') {
+            for (let key in state) {
+               formData.append(key, state[key])
+            }
+         }
 
          postData('assets/server.php', formData)
             .then(res => {
--- Learning JS/IrvasWindowsProject/src/js/modules/modals.js
+++ Learning JS/IrvasWindowsProject/src/js/modules/modals.js
@@ -1,49 +1,81 @@
-
 const modals = () => {
-   function bindModal(triggerSelector, modalSelector, closeSelector) {
+	function bindModal(
+		triggerSelector,
+		modalSelector,
+		closeSelector,
+		closeClickOverlay = true
+	) {
+		const trigger = document.querySelectorAll(triggerSelector),
+			modal = document.querySelector(modalSelector),
+			close = document.querySelector(closeSelector),
+			windows = document.querySelectorAll('[data-modal]');
 
-      const trigger = document.querySelectorAll(triggerSelector)
-      const modal = document.querySelector(modalSelector)
-      const close = document.querySelector(closeSelector)
+		trigger.forEach((item) => {
+			item.addEventListener('click', (e) => {
+				if (e.target) {
+					e.preventDefault();
+				}
 
-      trigger.forEach(item => {
-         item.addEventListener('click', (e) => {
-            if (e.target) {
-               e.preventDefault()
-            }
-            
-            modal.style.display = 'block'
-            // document.body.classList.add('modal-open')
-            document.body.style.overflow = 'hidden'
-         })
-      })
+				windows.forEach((item) => {
+					item.style.display = 'none';
+				});
 
-      close.addEventListener('click', () => {
-         modal.style.display = 'none'
-         // document.body.classList.remove('modal-open')
-         document.body.style.overflow = ''
-      })
+				modal.style.display = 'block';
+				document.body.style.overflow = 'hidden';
+				// document.body.classList.add('modal-open');
+			});
+		});
 
-      modal.addEventListener('click', (e) => {
-         if (e.target === modal) {
-            modal.style.display = 'none'
-            // document.body.classList.remove('modal-open')
-            document.body.style.overflow = ''
-         }
-      })
-   }
-   
-   function showModalByTime(selector, time) {
-      setTimeout(() => {
-         document.querySelector(selector).style.display = 'block'
-         document.body.style.overflow = 'hidden'
-      }, time)
-   }
+		close.addEventListener('click', () => {
+			windows.forEach((item) => {
+				item.style.display = 'none';
+			});
 
-   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close')
-   bindModal('.phone_link', '.popup', '.popup .popup_close')
-   showModalByTime('.popup', 60000)
-}
+			modal.style.display = 'none';
+			document.body.style.overflow = '';
+			// document.body.classList.remove('modal-open');
+		});
 
+		modal.addEventListener('click', (e) => {
+			if (e.target === modal && closeClickOverlay) {
+				windows.forEach((item) => {
+					item.style.display = 'none';
+				});
 
-export default modals
+				modal.style.display = 'none';
+				document.body.style.overflow = '';
+				// document.body.classList.remove('modal-open');
+			}
+		});
+	}
+
+	function showModalByTime(selector, time) {
+		setTimeout(function () {
+			document.querySelector(selector).style.display = 'block';
+			document.body.style.overflow = 'hidden';
+		}, time);
+	}
+
+	bindModal(
+		'.popup_engineer_btn',
+		'.popup_engineer',
+		'.popup_engineer .popup_close'
+	);
+	bindModal('.phone_link', '.popup', '.popup .popup_close');
+	bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+	bindModal(
+		'.popup_calc_button',
+		'.popup_calc_profile',
+		'.popup_calc_profile_close',
+		false
+	);
+	bindModal(
+		'.popup_calc_profile_button',
+		'.popup_calc_end',
+		'.popup_calc_end_close',
+		false
+	);
+	showModalByTime('.popup', 60000);
+};
+
+export default modals;
