4414e25b3d207a20342c2fecff53b0b94b6cc655
Nexus <egor2001grashin@gmail.com>
2023-03-13 01:23:34 +0300

    base types in ts

--- Learning TS/test/index.ts
+++ Learning TS/test/index.ts
@@ -1,2 +1,12 @@
-const str = 'str';
-console.log(str)
+
+// String: '', "", ``
+// Number: NaN, infinity, 10, 0.5, 0.0001, -50, 4e10
+// Boolean: true, false
+
+let isBirthday: boolean = true
+let age: number = 40
+let userName: string = 'John'
+
+if (isBirthday) {
+   console.log(`Congrats ${userName}, age: ${age + 1}`)
+}
