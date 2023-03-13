72bd8ae5e60a871940b69e41c11b748cd43ebc23
Nexus <egor2001grashin@gmail.com>
2023-03-14 00:54:44 +0300

    type 'never'

--- Learning TS/test/index.ts
+++ Learning TS/test/index.ts
@@ -1,36 +1,20 @@
 
-// String: '', "", ``
-// Number: NaN, infinity, 10, 0.5, 0.0001, -50, 4e10
-// Boolean: true, false
-
 let isBirthdayData: boolean = true
 let ageData: number = 40
 let userNameData: string = 'John'
 
-function logBrtMsg(isBirthday: boolean, userName: string, age: number): void {
-   if (isBirthday) {
-      console.log(`Congrats ${userName}, age: ${age + 1}`)
-   }
+const createError = (msg: string): never => { 
+   throw new Error(msg)
 }
 
-function logBrtMsg2(isBirthday: boolean, userName: string, age: number): string {
+function logBrtMsg(isBirthday: boolean, userName: string, age: number): string {
    if (isBirthday) {
       return `Congrats ${userName}, age: ${age + 1}`
-   } else {
-      return 'Error'
+   } else if (isBirthday === false) {
+      return 'Too bad'
    }
+   return createError('Error')
 }
-
-const arrowlogBrtMsg = (isBirthday: boolean, userName: string, age: number): string => {
-   if (isBirthday) {
-      return `Congrats ${userName}, age: ${age + 1}`
-   } else {
-      return 'Error'
-   }
-}
-
 
 
 logBrtMsg(isBirthdayData, userNameData, ageData)
-logBrtMsg2(isBirthdayData, userNameData, ageData)
-arrowlogBrtMsg(isBirthdayData, userNameData, ageData)
