31c9c555b61778bf53442c64a535c3e89269abb1
Nexus <egor2001grashin@gmail.com>
2023-03-13 20:40:04 +0300

    using types in functions

--- Learning TS/test/index.js
+++ Learning TS/test/index.js
@@ -1,2 +1,30 @@
-var str = 'str';
-console.log(str);
+// String: '', "", ``
+// Number: NaN, infinity, 10, 0.5, 0.0001, -50, 4e10
+// Boolean: true, false
+var isBirthdayData = true;
+var ageData = 40;
+var userNameData = 'John';
+function logBrtMsg(isBirthday, userName, age) {
+    if (isBirthday) {
+        console.log("Congrats ".concat(userName, ", age: ").concat(age + 1));
+    }
+}
+function logBrtMsg2(isBirthday, userName, age) {
+    if (isBirthday) {
+        return "Congrats ".concat(userName, ", age: ").concat(age + 1);
+    }
+    else {
+        return 'Error';
+    }
+}
+var arrowlogBrtMsg = function (isBirthday, userName, age) {
+    if (isBirthday) {
+        return "Congrats ".concat(userName, ", age: ").concat(age + 1);
+    }
+    else {
+        return 'Error';
+    }
+};
+logBrtMsg(isBirthdayData, userNameData, ageData);
+logBrtMsg2(isBirthdayData, userNameData, ageData);
+arrowlogBrtMsg(isBirthdayData, userNameData, ageData);
--- Learning TS/test/index.ts
+++ Learning TS/test/index.ts
@@ -3,10 +3,34 @@
 // Number: NaN, infinity, 10, 0.5, 0.0001, -50, 4e10
 // Boolean: true, false
 
-let isBirthday: boolean = true
-let age: number = 40
-let userName: string = 'John'
+let isBirthdayData: boolean = true
+let ageData: number = 40
+let userNameData: string = 'John'
 
-if (isBirthday) {
-   console.log(`Congrats ${userName}, age: ${age + 1}`)
+function logBrtMsg(isBirthday: boolean, userName: string, age: number): void {
+   if (isBirthday) {
+      console.log(`Congrats ${userName}, age: ${age + 1}`)
+   }
 }
+
+function logBrtMsg2(isBirthday: boolean, userName: string, age: number): string {
+   if (isBirthday) {
+      return `Congrats ${userName}, age: ${age + 1}`
+   } else {
+      return 'Error'
+   }
+}
+
+const arrowlogBrtMsg = (isBirthday: boolean, userName: string, age: number): string => {
+   if (isBirthday) {
+      return `Congrats ${userName}, age: ${age + 1}`
+   } else {
+      return 'Error'
+   }
+}
+
+
+
+logBrtMsg(isBirthdayData, userNameData, ageData)
+logBrtMsg2(isBirthdayData, userNameData, ageData)
+arrowlogBrtMsg(isBirthdayData, userNameData, ageData)
