7791949f4fca86ec5b2f9b4171bade969ac1c52f
Nexus <egor2001grashin@gmail.com>
2023-03-15 20:32:35 +0300

    'symbol' and 'bigint' types in ts

--- Learning TS/test/test.ts
+++ Learning TS/test/test.ts
@@ -18,3 +18,21 @@ function getRndData() {
 const data = getRndData()
 const trimmedData = data.trim() // error, coz can be null
 const trimmedDataWorks = data ? data.trim() : null
+
+
+
+let id: symbol = Symbol('id')
+
+const myData = {
+   [id]: 1
+} 
+
+console.log(myData[id])
+
+
+let num1: bigint = 1n
+let num2: bigint = 2n
+
+console.log(num1 + num2) // 3n
+console.log(num1 + 5) // error
+
